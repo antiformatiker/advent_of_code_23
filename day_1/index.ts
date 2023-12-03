69e4aaded492f7fdb573168951e9fce53c42ad94
Antiformatiker <antiformatiker@users.noreply.github.com>
2023-12-03 23:32:52 +0100

    feat: add day 1 solution

---
+++ day_1/index.ts
@@ -0,0 +1,14 @@
+const first_num = (str: string) => str.split('').find(c => /\d/.test(c));
+const last_num = (str: string) => str.split('').reverse().find(c => /\d/.test(c));
+const combine_num = (x?: string, y?: string) => (!x || !y) ? 0 : parseInt(x + y)
+
+export async function day_1() {
+    const file = Bun.file(`${import.meta.dir}/input.txt`);
+    const content = await file.text();
+    return content
+            .split(/\r?\n|\r|\n/g)
+            .map(x => x.trim())
+            .filter(x => x !== '')
+            .map(x => combine_num(first_num(x), last_num(x)))
+            .reduce((x, y) => x + y, 0);
+}
---
+++ index.ts
@@ -0,0 +1,4 @@
+import { day_1 } from './day_1';
+
+const result = await day_1()
+console.log(`Day 1: ${result}`);
